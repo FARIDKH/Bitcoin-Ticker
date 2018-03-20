095c59f7df9ac30f03e6c6c9101fed9b0e8eb161
Farid Qanbarov <faridqanbarov@Farids-MacBook-Air.local>
2018-03-20 21:34:10 +0100

    First app commit

---
+++ BitcoinTicker/Model/BitcoinDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  BitcoinDataModel.swift
+//  BitcoinTicker
+//
+//  Created by Farid Qanbarov on 3/16/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
